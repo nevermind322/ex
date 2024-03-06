e9f4820e9447c063057166275d2facd929533bda
nevermind322 <81963698+nevermind322@users.noreply.github.com>
2024-03-06 14:20:36 +0300

    update delegation.kt

--- src/delegation.kt
+++ src/delegation.kt
@@ -6,17 +6,18 @@ interface Base {
     
 }
 
-class Der1 : Base {
+class Der1(val name : String) : Base {
+
     override fun g() {
-        println("g from der1")
+        println("g from $name")
     }
     
     override fun f() {
-        println("f from der1")
+        println("f from $name")
     }
 }
 
-class Der2( private val proxy : Base) : Base by proxy {
+class Der2(  proxy : Base) : Base by proxy {
     
     override fun g() {
         println("g from der2")
@@ -28,7 +29,7 @@ class Der2( private val proxy : Base) : Base by proxy {
 fun main() {
     
     
-    val der1 = Der1()
+    val der1 = Der1("foo")
     val der2 = Der2(der1)
     
     der2.f()
